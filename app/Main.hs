33987cae15575b4e6f704e0385838c9d8e6ba3d3
Dustin Sallings <dustin@spy.net>
2020-05-03 12:42:35 -0700

    Generate client IDs when clients don't bother.

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,8 @@ import           Data.Conduit             (runConduit, (.|))
 import           Data.Conduit.Attoparsec  (conduitParser, sinkParser)
 import qualified Data.Conduit.Combinators as C
 import           Data.Conduit.Network     (AppData, appSink, appSource, runTCPServer, serverSettings)
+import qualified Data.UUID                as UUID
+import           System.Random            (randomIO)
 import           UnliftIO                 (Async (..), MonadUnliftIO (..), async, cancel, link)
 
 import qualified Network.MQTT.Types       as T
@@ -32,19 +34,20 @@ dispatch _ x = fail ("unhandled: " <> show x)
 handleConnection :: forall m. (MonadLogger m, MonadUnliftIO m, MonadFail m, E.MonadMask m, E.MonadThrow m) => AppData -> MQTTD m ()
 handleConnection ad = runConduit $ do
   ch :: TChan T.MQTTPkt <- liftIO newTChanIO
-  cpkt@(T.ConnPkt _ pl) <- appSource ad .| sinkParser T.parseConnect
+  (cpkt@(T.ConnPkt _ pl), genedID) <- ensureID =<< appSource ad .| sinkParser T.parseConnect
   o <- lift . async $ processOut pl ch
 
-  lift $ E.finally (runIn pl ch cpkt o) (teardown o ch cpkt)
+  lift $ E.finally (runIn pl ch cpkt genedID o) (teardown o ch cpkt)
 
   where
-    runIn :: T.ProtocolLevel -> TChan T.MQTTPkt -> T.MQTTPkt -> Async () -> MQTTD m ()
-    runIn pl ch (T.ConnPkt req@T.ConnectRequest{..} _) o = do
+    runIn :: T.ProtocolLevel -> TChan T.MQTTPkt -> T.MQTTPkt -> Maybe BL.ByteString -> Async () -> MQTTD m ()
+    runIn pl ch (T.ConnPkt req@T.ConnectRequest{..} _) nid o = do
       logInfoN ("A connection is made " <> tshow req)
       link o
       -- Register and accept the connection
       _ <- registerClient req ch o
-      sendPacketIO ch (T.ConnACKPkt $ T.ConnACKFlags False T.ConnAccepted mempty)
+      let cprops = [ T.PropAssignedClientIdentifier i | Just i <- [nid] ]
+      sendPacketIO ch (T.ConnACKPkt $ T.ConnACKFlags False T.ConnAccepted cprops)
 
       runConduit $ appSource ad
         .| conduitParser (T.parsePacket pl)
@@ -64,6 +67,12 @@ handleConnection ad = runConduit $ do
       case _lastWill of
         Nothing               -> pure ()
         Just (T.LastWill{..}) -> broadcast (blToText _willTopic) _willMsg _willRetain _willQoS
+
+    ensureID (T.ConnPkt c@T.ConnectRequest{_connID=""} pl) = do
+      nid <- BL.fromStrict . UUID.toASCIIBytes <$> liftIO randomIO
+      logDebugN ("Generating ID for anonymous client: " <> tshow nid)
+      pure (T.ConnPkt c{T._connID=nid} pl, Just nid)
+    ensureID x = pure (x, Nothing)
 
 main :: IO ()
 main = do
