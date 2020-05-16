638164ec9dd324b723af04925ec7818f41c7eabb
Dustin Sallings <dustin@spy.net>
2020-05-16 13:57:56 -0700

    Log listeners on startup.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           Control.Monad            (void)
 import           Control.Monad.Catch      (MonadMask (..))
-import           Control.Monad.Logger     (MonadLogger (..), runStderrLoggingT)
+import           Control.Monad.Logger     (MonadLogger (..), logInfoN, runStderrLoggingT)
 import           Data.Conduit.Network     (runTCPServer, serverSettings)
 import           Data.Conduit.Network.TLS (runGeneralTCPServerTLS, tlsConfig)
 import qualified Network.WebSockets       as WS
@@ -11,11 +11,18 @@ import           UnliftIO                 (MonadUnliftIO (..), async, waitAnyCan
 import           MQTTD
 import           MQTTD.Conduit
 import           MQTTD.Config
+import           MQTTD.Util
 
 runListener :: (MonadUnliftIO m, MonadLogger m, MonadFail m, MonadMask m) => Listener -> MQTTD m ()
-runListener (MQTTListener a p) = withRunInIO $ \unl -> runTCPServer (serverSettings p a) (unl . tcpApp)
-runListener (WSListener a p) = withRunInIO $ \unl -> WS.runServer a p (unl . webSocketsApp)
-runListener (MQTTSListener a p c k) = withRunInIO $ \unl -> runGeneralTCPServerTLS (tlsConfig a p c k) (unl . tcpApp)
+runListener (MQTTListener a p) = do
+  logInfoN ("Starting mqtt service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> runTCPServer (serverSettings p a) (unl . tcpApp)
+runListener (WSListener a p) = do
+  logInfoN ("Starting websocket service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> WS.runServer a p (unl . webSocketsApp)
+runListener (MQTTSListener a p c k) = do
+  logInfoN ("Starting mqtts service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> runGeneralTCPServerTLS (tlsConfig a p c k) (unl . tcpApp)
 
 main :: IO ()
 main = do
