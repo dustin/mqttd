592eaf52cbba8588f5be6dc9eebf4ce82a6b1c90
Dustin Sallings <dustin@spy.net>
2020-05-18 13:39:17 -0700

    Document and be clearer on types.

--- src/MQTTD/SubTree.hs
+++ src/MQTTD/SubTree.hs
@@ -1,12 +1,17 @@
-module MQTTD.SubTree where
+module MQTTD.SubTree (
+  SubTree, modify, add, find, findMap, flatten, fromList
+  ) where
 
-import           Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import           Data.Text       (Text, intercalate, splitOn)
+import           Data.Map.Strict    (Map)
+import qualified Data.Map.Strict    as Map
+import           Data.Text          (intercalate, splitOn)
 
+import           Network.MQTT.Topic (Filter, Topic)
+
+-- | MQTT Topic Subscription tree.
 data SubTree a = SubTree {
   subs     :: a,
-  children :: Map Text (SubTree a)
+  children :: Map Filter (SubTree a)
   } deriving (Show, Eq)
 
 instance Functor SubTree where
@@ -24,30 +29,41 @@ instance Semigroup a => Semigroup (SubTree a) where
 instance Monoid a => Monoid (SubTree a) where
   mempty = SubTree mempty mempty
 
-modify :: Monoid a => Text -> (a -> a) -> SubTree a -> SubTree a
+-- | Modify a subscription for the given filter.
+--
+-- This will create structure along the path and will not clean up any
+-- unused structure.
+modify :: Monoid a => Filter -> (a -> a) -> SubTree a -> SubTree a
 modify top f = go (splitOn "/" top)
   where
     go [] n@SubTree{..}     = n{subs=f subs}
     go (x:xs) n@SubTree{..} = n{children=Map.alter (fmap (go xs) . maybe (Just mempty) Just) x children}
 
-add :: Monoid a => Text -> a -> SubTree a -> SubTree a
+-- | Add a value at the given filter path.
+add :: Monoid a => Filter -> a -> SubTree a -> SubTree a
 add top i = modify top (i<>)
 
-find :: Monoid a => Text -> SubTree a -> a
-find top = go (splitOn "/" top)
+-- | Find all matching subscribers
+findMap :: Monoid m => Topic -> (a -> m) -> SubTree a -> m
+findMap top f = go (splitOn "/" top)
   where
-    go [] SubTree{subs} = subs
-    go (x:xs) SubTree{children} = maybe mempty (go xs) (Map.lookup x children)
-                               <> maybe mempty (go xs) (Map.lookup "+" children)
-                               <> maybe mempty subs    (Map.lookup "#" children)
+    go [] SubTree{subs} = f subs
+    go (x:xs) SubTree{children} = maybe mempty (go xs)    (Map.lookup x children)
+                               <> maybe mempty (go xs)    (Map.lookup "+" children)
+                               <> maybe mempty (f . subs) (Map.lookup "#" children)
+
+-- | Find subscribers of a given topic.
+find :: Monoid a => Topic -> SubTree a -> a
+find top = findMap top id
 
 -- | flatten a SubTree to a list of (topic,a) pairs.
--- Note this will return 'mempty' a where there are none.
-flatten :: SubTree a -> [(Text, a)]
+-- Note this will return 'mempty' in paths along subscription paths.
+flatten :: SubTree a -> [(Filter, a)]
 flatten = Map.foldMapWithKey (\k sn -> go [k] sn) . children
   where
     go ks SubTree{..} = [(intercalate "/" (reverse ks), subs)]
                         <> Map.foldMapWithKey (\k sn -> go (k:ks) sn) children
 
-fromList :: Monoid a => [(Text, a)] -> SubTree a
+-- | Construct a SubTree from a list of filters and subscribers.
+fromList :: Monoid a => [(Filter, a)] -> SubTree a
 fromList = foldr (uncurry add) mempty
