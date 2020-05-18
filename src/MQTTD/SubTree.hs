bee7e39cdd1946fa065b1797a3d02910dea12ee1
Dustin Sallings <dustin@spy.net>
2020-05-17 23:21:08 -0700

    Created SubTree to efficiently manage subscriptions.

---
+++ src/MQTTD/SubTree.hs
@@ -0,0 +1,39 @@
+module MQTTD.SubTree where
+
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Data.Text       (Text, splitOn)
+
+data SubTree a = SubTree {
+  subs     :: [a],
+  children :: Map Text (SubTree a)
+  } deriving (Show, Eq)
+
+instance Functor SubTree where
+  fmap f SubTree{..} = SubTree (f <$> subs) ((fmap.fmap) f children)
+
+instance Foldable SubTree where
+  foldMap f SubTree{..} = foldMap f subs <> foldMap (foldMap f) children
+
+modifySub :: Text -> ([a] -> [a]) -> SubTree a -> SubTree a
+modifySub top f = go (splitOn "/" top)
+  where
+    go [] n@SubTree{..} = n{subs=f subs}
+    go (x:xs) n@SubTree{..} = n{children=Map.alter (fmap (go xs) . withChild) x children}
+      where
+        withChild Nothing = Just (SubTree mempty mempty)
+        withChild (Just n') = Just n'
+
+addSub :: Text -> a -> SubTree a -> SubTree a
+addSub top i = modifySub top (i:)
+
+unsub :: Eq a => Text -> a -> SubTree a -> SubTree a
+unsub top i = modifySub top (filter (/= i))
+
+findSubd :: Text -> SubTree a -> [a]
+findSubd top = go (splitOn "/" top)
+  where
+    go [] SubTree{subs} = subs
+    go (x:xs) SubTree{children} = maybe [] (go xs) (Map.lookup x children)
+                               <> maybe [] (go xs) (Map.lookup "+" children)
+                               <> maybe [] (subs) (Map.lookup "#" children)
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,10 +5,13 @@ import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck    as QC
 
 import           Data.Either              (isLeft, isRight)
+import           Data.List                (sort)
 import qualified Data.Map.Strict          as Map
+import           Data.Text                (Text, pack)
 
 import           MQTTD
 import           MQTTD.Config
+import           MQTTD.SubTree
 
 testConfigFiles :: Assertion
 testConfigFiles =
@@ -40,6 +43,29 @@ instance EqProp ListenerOptions where (=-=) = eq
 instance Arbitrary ListenerOptions where
   arbitrary = ListenerOptions <$> arbitrary
 
+instance Eq a => EqProp (SubTree a) where (=-=) = eq
+
+instance Arbitrary Text where
+  arbitrary = pack . getPrintableString <$> arbitrary
+
+instance Arbitrary a => Arbitrary (SubTree a) where
+  arbitrary = SubTree <$> arbitrary <*> arbitrary
+
+testSubTree :: Assertion
+testSubTree =
+    mapM_ aTest [
+    ("a/b/c", ["a/b/c", "a/+/c", "a/+/+", "+/+/+", "+/b/c", "#", "a/#"]),
+    ("a/d/c", ["a/+/c", "a/+/+", "+/+/+", "#", "a/#"]),
+    ("b/b/c", ["+/+/+", "+/b/c", "#", "b/#"]),
+    ("b/real", ["#", "b/#"]),
+    ("a/b/x", ["#","+/+/+","a/#","a/+/+"])]
+
+  where
+    someSubs = foldr (\x -> addSub x x) (SubTree mempty mempty) [
+      "a/b/c", "a/+/c", "a/+/+", "+/+/+", "+/b/c",
+      "#", "a/#", "b/#"]
+    aTest (f,w) = assertEqual (show f) (sort w) (sort $ findSubd f someSubs)
+
 testACLs :: Assertion
 testACLs = mapM_ aTest [
   ([], "empty/stuffs", isRight),
@@ -57,6 +83,13 @@ tests :: [TestTree]
 tests = [
   testCase "config files" testConfigFiles,
   testCase "ACLs" testACLs,
+
+  testCase "subtree" testSubTree,
+  {-  Arbitrary generates absurd trees right now.  I'll fix that up tomorrow.
+  testGroup "subtree properties" [
+      testProperties "functor" (unbatch $ functor (undefined :: SubTree (Int, Int, Int)))
+      ],
+   -}
 
   testGroup "listener properties" [
       testProperties "semigroup" (unbatch $ semigroup (undefined :: ListenerOptions, undefined :: Int)),
