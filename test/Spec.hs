3446d55256caf8365f50e903fbe61fcf09eafea7
Dustin Sallings <dustin@spy.net>
2020-05-29 20:51:07 -0700

    Generate arbitrary topics with some arbitrary matches for SubTree testing.

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,7 +12,8 @@ import           Data.List                (intercalate, sort)
 import qualified Data.Map.Strict          as Map
 import           Data.Monoid              (Sum (..))
 import           Data.Set                 (Set)
-import           Data.Text                (Text, pack)
+import           Data.Text                (Text)
+import qualified Data.Text                as Text
 
 import           MQTTD
 import           MQTTD.Config
@@ -58,7 +59,7 @@ newtype ATopic = ATopic { unTopic :: Text } deriving (Eq, Show)
 instance Arbitrary ATopic where
   arbitrary = do
     n <- choose (1, 6)
-    ATopic . pack . intercalate "/" <$> vectorOf n seg
+    ATopic . Text.pack . intercalate "/" <$> vectorOf n seg
       where seg = choose (1, 8) >>= flip vectorOf (choose ('a', 'z'))
 
 instance (Monoid a, Arbitrary a, Eq a) => Arbitrary (SubTree a) where
@@ -99,6 +100,45 @@ testACLs = mapM_ aTest [
   where
     aTest (a,t,f) = assertBool (show (a, t)) $ f (authTopic t a)
 
+newtype Topic = Topic [Text] deriving (Show, Eq)
+
+instance Arbitrary Topic where
+  arbitrary = Topic <$> someSegs
+    where someSegs = choose (1,8) >>= flip vectorOf aSeg
+          aSeg = do
+            n <- choose (1,8)
+            Text.pack <$> vectorOf n aValidChar
+          aValidChar = elements (['A'..'Z'] <> ['a'..'z'] <> ['0'..'9'])
+
+  shrink (Topic x) = fmap Topic . shrinkList shrinkWord $ x
+    where shrinkWord = fmap Text.pack . shrink . Text.unpack
+
+data MatchingTopic = MatchingTopic Topic Topic deriving Eq
+
+instance Show MatchingTopic where
+  show (MatchingTopic t m) = concat ["MatchingTopic ",
+                                     (Text.unpack $ topicPath t), " -> ", (Text.unpack $ topicPath m)]
+
+instance Arbitrary MatchingTopic where
+  arbitrary = do
+    t@(Topic tsegs) <- arbitrary
+    reps <- vectorOf (length tsegs) (elements [id, const "+", const "#"])
+    let m = zipWith ($) reps tsegs
+    pure $ MatchingTopic t (Topic $ clean m)
+      where
+        clean []       = []
+        clean ("#":xs) = ["#"]
+        clean (x:xs)   = x : clean xs
+
+propSubTreeMapping :: [MatchingTopic] -> Bool
+propSubTreeMapping matches = all (\(t, m) -> m `elem` Sub.find t st) tp
+  where
+    tp = [(topicPath t, topicPath m) | (MatchingTopic t m) <- matches]
+    st = foldr (\(_,t) -> Sub.add t [t]) mempty tp
+
+topicPath :: Topic -> Text
+topicPath (Topic t) = Text.intercalate "/" t
+
 roundTrips :: (Eq a, Show a, Arbitrary a) => (a -> b) -> (b -> a) -> a -> Property
 roundTrips t f = f.t >>= (===)
 
@@ -110,6 +150,7 @@ tests = [
   testCase "subtree" testSubTree,
   testGroup "subtree properties" [
       testProperty "flatten/fromList" $ roundTrips (filter (not.null . snd) . Sub.flatten @(SubTree (Set Int))) Sub.fromList,
+      testProperty "finding" propSubTreeMapping,
       testProperties "functor" (unbatch $ functor (undefined :: SubTree ([Int], Int, Int))),
       testProperties "foldable" (unbatch $ foldable (undefined :: SubTree (Sum Int, Sum Int, Sum Int, Sum Int, Sum Int))),
       testProperties "traversable" (unbatch $ traversable (undefined :: SubTree (Sum Int, Sum Int, Sum Int))),
