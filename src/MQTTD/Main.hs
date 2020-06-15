1b3be24989bc8a6d4b9fe9293616691438465bf4
Dustin Sallings <dustin@spy.net>
2020-06-15 09:19:21 -0700

    Moved the main loop of main into a reusable module.
    This lets me spin up in-process servers for testing.

---
+++ src/MQTTD/Main.hs
@@ -0,0 +1,62 @@
+module MQTTD.Main where
+
+import           Control.Lens
+import           Control.Monad.Catch      (MonadMask (..))
+import           Control.Monad.Logger     (LogLevel (..), MonadLogger (..), filterLogger, logInfoN, runStderrLoggingT)
+import           Data.Conduit.Network     (runTCPServer, serverSettings)
+import           Data.Conduit.Network.TLS (runGeneralTCPServerTLS, tlsConfig)
+import           Data.Maybe               (fromMaybe)
+import           Database.SQLite.Simple   hiding (bind, close)
+import qualified Network.WebSockets       as WS
+import           UnliftIO                 (Async (..), MonadUnliftIO (..), async)
+
+import           MQTTD
+import           MQTTD.Conduit
+import           MQTTD.Config
+import           MQTTD.DB
+import           MQTTD.Types
+import           MQTTD.Util
+
+runListener :: (MonadUnliftIO m, MonadLogger m, MonadFail m, MonadMask m) => Listener -> MQTTD m ()
+runListener (MQTTListener a p _) = do
+  logInfoN ("Starting mqtt service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> runTCPServer (serverSettings p a) (unl . tcpApp)
+runListener (WSListener a p _) = do
+  logInfoN ("Starting websocket service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> WS.runServer a p (unl . webSocketsApp)
+runListener (MQTTSListener a p c k _) = do
+  logInfoN ("Starting mqtts service on " <> tshow a <> ":" <> tshow p)
+  withRunInIO $ \unl -> runGeneralTCPServerTLS (tlsConfig a p c k) (unl . tcpApp)
+
+
+runServer :: Config -> IO [Async ()]
+runServer conf@Config{..} = do
+
+  let baseAuth = _confDefaults `applyListenerOptions` Authorizer{
+        _authAnon = False,
+        _authUsers = _confUsers
+        }
+
+  withConnection (_persistenceDBPath _confPersist) $ \db -> do
+    initDB db
+    e <- newEnv baseAuth db
+    runStderrLoggingT . logfilt conf . runIO e $ do
+      sc <- async sessionCleanup
+      pc <- async retainerCleanup
+      dba <- async (runOperations $ statStore e)
+      st <- async publishStats
+      as <- async applyStats
+      restoreSessions
+      restoreRetained
+
+      ls <- traverse (async . runModified) _confListeners
+
+      pure (sc:pc:dba:st:as:ls)
+
+        where
+          logfilt Config{..} = filterLogger (\_ -> flip (if _confDebug then (>=) else (>)) LevelDebug)
+
+          runModified = modifyAuthorizer . applyListenerOptions . view listenerOpts <*> runListener
+
+          applyListenerOptions ListenerOptions{..} a@Authorizer{..} =
+            a{_authAnon=fromMaybe _authAnon _optAllowAnonymous}
