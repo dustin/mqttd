d27a584f94c890e0709990690d13bdebba5a9a45
Wesley Moore <wes@wezm.net>
2022-07-13 19:42:26 +1000

    Allow config file path to be supplied via CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,15 @@
 module Main where
 
 import           Control.Monad (void)
+import           System.Environment (getArgs)
 import           UnliftIO      (waitAnyCancel)
 
 import           MQTTD.Config
 import           MQTTD.Main
 
+configFilePath :: [String] -> String
+configFilePath [] = "mqttd.conf"
+configFilePath (path: _) = path
+
 main :: IO ()
-main = void . waitAnyCancel =<< runServer =<< parseConfFile "mqttd.conf"
+main = void . waitAnyCancel =<< runServer =<< parseConfFile . configFilePath =<< getArgs
