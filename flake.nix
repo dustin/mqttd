fd0769379f3d0a5aee7e6876a4986841025a35c1
Matt Ingenthron <ingenthr@cep.net>
2025-12-04 19:01:39 +0000

    Updating nix flake to include package artifacts.
    Prior to this change, the nix flake is for development only.
    It does not deliver a package in the output.  This change updates
    nix flakes to be a bit more modern and have an output.
    
    The result is that one can nix build directly.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,33 @@
 {
   description = "mqttd";
-  nixConfig.bash-prompt = "\[mqttd-dev\]$ ";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ haskellNix.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+
+        project = pkgs.haskell-nix.project {
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "mqttd";
+            src = ./.;
+          };
+          projectFileName = "stack.yaml";
+        };
+
+        flake = project.flake {};
+      in {
+        packages = {
+          default = flake.packages."mqttd:exe:mqttd";
+          mqttd = flake.packages."mqttd:exe:mqttd";
+        };
+
+        devShells.default = flake.devShell;
+      });
 }
